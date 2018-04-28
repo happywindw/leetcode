a85fe632cba736a8813f629bf02c0479604db9ad
happywindw <592595468@qq.com>
2018-04-28 18:31:49 +0800

    Refactor package name Medium to medium.

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -1,4 +1,4 @@
-package Medium;
+package medium;
 
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,4 @@
-package Medium;
+package medium;
 
 /**
  * Given a string, find the length of the longest substring without repeating characters.
